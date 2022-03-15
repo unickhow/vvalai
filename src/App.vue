f0347dec727edeb67012ca9940ee68efaa26996c
unick_how <unick.zhow@gmail.com>
2022-03-16 00:05:35 +0800

    🧹 chore: unocss and rebuild index page

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,52 @@
-<script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
+  <main class="mt-[5rem]">
+    <div class="container mx-auto">
+      <h1 class="flex items-center justify-center">
+        <i class="text-5xl i-carbon-chat-bot mr-2 color-[#ff7800]"></i>
+        <b>VAN starter template</b>
+      </h1>
+      <HelloWorld class="mb-16" slogan="Focus on idea instead scaffolding." />
+
+      <div class="text-center">
+        <h2 class="flex items-center justify-center color-[#2E4052]">
+          <i class="i-carbon-tool-box mr-2"></i>
+          <b>Prepacks</b>
+        </h2>
+        <p>{{ prepacks.join(', ') }}</p>
+      </div>
+
+      <div class="text-center flex justify-center mt-20">
+        <a
+          href="https://github.com/unickhow"
+          class="i-carbon-logo-github text-5 transition-colors color-[#666] hover:color-[#333]"></a>
+      </div>
+    </div>
+  </main>
 </template>
 
+<script setup lang="ts">
+import HelloWorld from './components/HelloWorld.vue'
+
+const prepacks = ref([
+  'Vite',
+  'Vue 3',
+  'Typescript',
+  'Unocss',
+  'Carbon Icons',
+])
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+html, body {
+  background: #fafafa;
+}
+</style>
+
+<style scoped>
+h1 b {
+  background: -webkit-linear-gradient(45deg, #FF7800, #2E4052);
+  -webkit-background-clip: text;
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
 }
 </style>
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
+import Unocss from 'unocss/vite'
+import transformerDirective from '@unocss/transformer-directives'
+import presetUno from '@unocss/preset-uno'
+import presetIcons from '@unocss/preset-icons'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,6 +12,13 @@ export default defineConfig({
     vue(),
     AutoImport({
       imports: ['vue']
+    }),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetIcons()
+      ],
+      transformers: [transformerDirective()]
     })
   ]
 })
