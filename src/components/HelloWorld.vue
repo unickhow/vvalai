39fc5fe4b2afb08beca208af9f7c59a4f3e4626d
unick_how <unick.zhow@gmail.com>
2022-03-17 23:47:19 +0800

    🧹 chore: add vueuse

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,10 +1,13 @@
 <template>
   <p class="text-center text-lg font-bold color-[#757575]">{{ slogan }}</p>
+  <small class="block text-center">X: {{ x }}, Y: {{ y }}</small>
 </template>
 
 <script setup lang="ts">
-defineProps<{ slogan: string }>()
+import { useMouse } from '@vueuse/core'
+const { x, y } = useMouse()
 
+defineProps<{ slogan: string }>()
 const count = ref(0)
 </script>
 
